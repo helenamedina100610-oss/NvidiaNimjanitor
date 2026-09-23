f625a953aa1816cb594c5a194e452731314a0961
helenamedina100610-oss <helena.medina100610@gmail.com>
2026-09-23 04:49:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const NVIDIA_API_KEY = process.env.NVIDIA_API_KEY || '';
 
 const DEFAULT_MODEL =
   process.env.NVIDIA_MODEL ||
-  moonshotai/kimi-k3;
+  'moonshotai/kimi-k3';
 
 // Thinking is enabled for the model,
 // but reasoning content is NOT shown to Janitor.
